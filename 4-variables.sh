23616ae531049842286d503e887400c9efce92a0
hai121212 <hhi323171@gmail.com>
2024-08-19 20:49:24 +0530

    hello script

--- 4-variables.sh
+++ 4-variables.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo "please enter username:"
-
 read username
 echo "please enter password:"
 read password
-echo "username is: $usename, password is: $password"
+echo "username is: $username, password is: $password"
