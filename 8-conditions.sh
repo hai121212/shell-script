2bdab08d4dc1cfba80dc7b877e0a7a66b57d169e
hai121212 <hhi323171@gmail.com>
2024-08-20 11:25:05 +0530

    hello script

--- 8-conditions.sh
+++ 8-conditions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 num=$1
-if [$num -gt 10]
+if [$number -gt 10]
 then
-    echo "given number $num is greather than 10"
+    echo "given number $number is greather than 10"
 else
-    echo"given number $num is less than 10"
+    echo"given number $number is less than 10"
 fi
